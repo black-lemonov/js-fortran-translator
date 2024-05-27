6de659f9b55e31a6c4425b8c9a3d2153fdea83af
Егор Парфинцов <egor.porf1@mail.ru>
2024-05-28 01:08:07 +0300

    lab1: add some js code samples

---
+++ tests/hello-world.js
@@ -0,0 +1,19 @@
+
+function main() {
+    var a = 2;
+    var pi = 3.14;
+    var c = 2 + 3;
+
+    if (a > 2) {
+        a = 2;
+    }
+
+    for (i = 0; i < 5; i = i + 1) {
+        a = 2;
+    }
+
+    while (c > 2) {
+        c = c + 1;
+    }
+    
+}
